2760f16bf781ccd4bc9bd2a9bccf1fc4685fea5f
gillianyue <gillianyue1999@hotmail.com>
2017-08-08 11:21:39 +0800

    Revert "Revert "Bullet Trials!""
    This reverts commit 4050671d20f4c7db05c89590e5935f7660540aba.

---
+++ Assets/Scripts/TrailFollowBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailFollowBall : MonoBehaviour {
+	
+	private GameObject myBullet;
+	public float lastingTime;
+	public DestroyByTime dbt;
+
+	void Start () {
+		dbt.enabled = false;
+	}
+		
+	void Update () {
+		if (myBullet != null) {
+			transform.position = myBullet.transform.position;
+		} else {
+			if (!dbt.enabled) {
+				dbt.enabled = true;
+			}
+		}
+	}
+
+	public void setMyBall(GameObject b){
+		myBullet = b;
+	}
+
+
+}
