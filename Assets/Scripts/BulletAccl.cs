eee035982e0b9bf689da20f4b055dd1fa4578913
gillianyue <gillianyue1999@hotmail.com>
2017-08-08 11:15:47 +0800

    Bullet Trials!
    Beautiful rainbow bullet trail VFX

--- Assets/Scripts/BulletAccl.cs
+++ Assets/Scripts/BulletAccl.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class BulletAccl : MonoBehaviour {
 	
 	public float accl;
+	public GameObject trail;
 
-	// Use this for initialization
 	void Start () {
-	
+		GameObject t = trail;
+		t = Instantiate (trail, gameObject.transform.position,
+			trail.transform.rotation) as GameObject;
+		t.GetComponent<TrailFollowBall> ().setMyBall (gameObject);
+
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
+		//acceleration
 		GetComponent<Rigidbody> ().velocity += new Vector3 
 			(GetComponent<Rigidbody> ().velocity.x * accl, 0, 
 				GetComponent<Rigidbody> ().velocity.z *accl);
+
+
 	}
 }
--- Assets/Scripts/DestroyByTime.cs
+++ Assets/Scripts/DestroyByTime.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class DestroyByTime : MonoBehaviour {
 
-	public int seconds;
+	public float seconds;
 
 	// Use this for initialization
 	void Start () {
