408bb23cae6cb64a61a688dc2345dff0134d7762
Gillian Yue <gillianyue1999@hotmail.com>
2019-08-11 22:41:53 +0800

    some progress

---
+++ Assets/Scripts/BossBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * abstract base class for levelscripts, which contain custom functions for special events in each level
+ */
+public abstract class BossBehavior : MonoBehaviour
+{
+
+    public int life = 1000;
+    public int attack;
+
+    public float sizeScale; //same as paintball, base scale to be multiplied with global
+    public float colliderScale; //multiplied to the original generated polygon 2d collider shape to resize
+
+    protected GameControl gameControl;
+    protected GameFlow gameFlow;
+    protected CustomEvents customEvents;
+
+    public void Start()
+    {
+        customEvents = gameObject.GetComponent<CustomEvents>();
+        GameObject gameController = GameObject.FindWithTag("GameController");
+        gameControl = gameController.GetComponent<GameControl>();
+        gameFlow = gameController.GetComponent<GameFlow>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void setSizeScale(float sScale)
+    {
+        sizeScale = sScale;
+
+        if (Global.scaleRatio != 0)
+        {
+            transform.localScale = new Vector3(sizeScale * Global.scaleRatio,
+                sizeScale * Global.scaleRatio, sizeScale * Global.scaleRatio);
+        }
+    }
+
+    public void setColliderScale(float cScale)
+    {
+        Vector2[] colliderPoints = GetComponent<PolygonCollider2D>().points;
+        Vector2[] scaledPoints = new Vector2[colliderPoints.Length];
+        for (int p = 0; p < colliderPoints.Length; p++)
+        {
+            Vector2 np = GetComponent<PolygonCollider2D>().points[p] * colliderScale;
+            scaledPoints[p] = np;
+        }
+        GetComponent<PolygonCollider2D>().SetPath(0, scaledPoints);
+    }
+
+
+}
---
+++ Assets/Scripts/PirateShip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateShip : BossBehavior
+{
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        life = 50;
+        attack = 3; //cannonball damage
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
